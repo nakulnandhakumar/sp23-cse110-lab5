53a6041f5b2a60a84730d4938aa0f22d92a1c783
nakulnandhakumar <nakul@nandhakumar.net>
2023-05-02 17:02:46 -0700

    expose done

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,61 @@
 // expose.js
+var hornSelect;
+var hornImage;
+var volumeControls;
+var volumeInput;
+var audio;
+var button;
+var jsConfetti;
 
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  hornSelect = document.getElementById("horn-select");
+  hornImage = document.querySelector("#expose img");
+  volumeControls = document.querySelector("#volume-controls img");
+  volumeInput = document.getElementById("volume");
+  audio = document.querySelector("audio.hidden");
+  button = document.querySelector("button");
+
+  volumeInput.addEventListener("input", changeVolumeImage);
+  hornSelect.addEventListener("input", changeHorn);
+  button.addEventListener("click", playAudio);
+  jsConfetti = new JSConfetti();
+} 
+
+function changeVolumeImage() {
+  if (volumeInput.value == 0) {
+    volumeControls.src = "assets/icons/volume-level-0.svg";
+  } else if (volumeInput.value < 33) {
+    volumeControls.src = "assets/icons/volume-level-1.svg";
+  } else if (volumeInput.value < 66) {
+    volumeControls.src = "assets/icons/volume-level-2.svg";
+  } else {
+    volumeControls.src = "assets/icons/volume-level-3.svg";
+  }
+  audio.volume = volumeInput.value/100;
+}
+
+function changeHorn() {
+  if (hornSelect.value == 'air-horn') {
+    hornImage.src = "assets/images/air-horn.svg";
+    audio.src = "assets/audio/air-horn.mp3";
+  } else if (hornSelect.value == 'car-horn') {
+    hornImage.src = "assets/images/car-horn.svg";
+    audio.src = "assets/audio/car-horn.mp3";
+  } else if (hornSelect.value == 'party-horn') {
+    hornImage.src = "assets/images/party-horn.svg";
+    audio.src = "assets/audio/party-horn.mp3";
+  } else {
+    hornImage.src = "assets/images/no-image.png";
+    audio.src = "";
+  }
+}
+
+function playAudio() {
+  if (hornSelect.value == 'party-horn') {
+    jsConfetti.addConfetti();
+    jsConfetti.clearCanvas();
+  }
+  audio.play();
 }
